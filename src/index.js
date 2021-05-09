9619ee86a3b42e2d62fb6c8300d05e301efa7877
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-09 10:31:44 +0100

    sass,fontawesome, uuid, components setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+//Importing our styles
+import './styles/app.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(

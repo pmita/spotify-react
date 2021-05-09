b81a570c6c90795130bcc5d86d9840ffdf0badfa
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-09 11:41:53 +0100

    player component setup also

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 //Importing our components
 import Library from './components/Library';
 import Player from './components/Player';
@@ -7,16 +7,32 @@ import songs from './songs_db';
 
 function App() {
   /*
+    Let's setup our html reference
+  */
+ const audioRef = useRef(null);
+  /*
     Let's setup our State
   */
  const [allSongs, setAllSongs] = useState(songs);
+ const [currentSong, setCurrentSong] = useState(allSongs[0]);
   return (
     <div className="App">
       <Library 
         allSongs={allSongs}
         setAllSongs={setAllSongs}
       />
-      <Player />
+      <Player 
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+      />
+
+      <audio 
+        ref={audioRef}
+        src={currentSong.audio}
+      >
+      </audio>
+      
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,40 @@
 import React from 'react';
+//Importing evrything Fontawesome related
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faPlay, faPause, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
-const Player = () => {
+
+const Player = ({currentSong, setCurrentSong, audioRef}) => {
+    /*
+        Adding event Listeners
+    */
+   const playSongHandler = () => {
+       console.log(audioRef.current.play);
+       audioRef.current.play();
+   }
     return(
         <div className="player">
-            <h2>Player</h2>
+            <div className="controls">
+                <FontAwesomeIcon 
+                    icon={faAngleLeft}
+                    size="2x"
+                />
+                <FontAwesomeIcon 
+                    onClick = {playSongHandler}
+                    className="play-icon"
+                    icon={faPlay}
+                    size="4x"
+                />
+                <FontAwesomeIcon 
+                    icon={faAngleRight}
+                    size="2x"
+                />
+            </div>
+            <div className="song-info">
+                <p>0</p>
+                <input type="range" />
+                <p>100</p>
+            </div>
         </div>
     );
 }
